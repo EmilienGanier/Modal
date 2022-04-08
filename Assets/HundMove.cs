7f3748a78e5ff9bfe7c29f849414da07c28c9ec6
sooo21 <solene.tsang@gmail.com>
2022-04-08 14:57:34 +0200

    sans erreur c'est mieux

--- Assets/HundMove.cs
+++ Assets/HundMove.cs
@@ -7,15 +7,22 @@ public class HundMove : MonoBehaviour
     public Rigidbody2D body;
     public bool goRight;
     public bool triggered;
-    public speed;
+    public float speed;
     private bool mustGoBack;
-    private bool 
+    private bool isWaiting;
     // Start is called before the first frame update
     void Start()
     {
         triggered = false;
-        if (!goRight) speed = -speed;
-    }
+        isWaiting = true;
+        mustGoBack = false;
+        if (!goRight){
+             speed = -speed;
+             Vector3 theScale = transform.localScale;
+             theScale.x *= -1;
+             transform.localScale = theScale;
+             }
+        }
 
     // Update is called once per frame
     void Update()
@@ -26,7 +33,13 @@ public class HundMove : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.tag == "Ground") grounded = true;
+        if (other.collider.tag == "WaitPlace") isWaiting = true;
+        if (ither.collider.tag == "Goal") mustGoBack = true;
+    }
+    void OncollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "WaitPlace") isWaiting = false;
+        if (ither.collider.tag == "Goal") mustGoBack = false;
     }
 
 
