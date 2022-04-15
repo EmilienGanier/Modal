16cc20cb7cd40cd1f410969fe40e5209f39435cc
EmilienGanier <emilien.ganier@laposte.net>
2022-04-15 17:36:51 +0200

    level1 saauf ladder

--- Assets/Script/Ennemies/KeeperLvl1.cs
+++ Assets/Script/Ennemies/KeeperLvl1.cs
@@ -6,6 +6,7 @@ public class KeeperLvl1 : MonoBehaviour
 {
 
     private Transform bolchiPos;
+    public bool notBlocked = true;
 
     // Start is called before the first frame update
     void Awake()
@@ -22,8 +23,13 @@ public class KeeperLvl1 : MonoBehaviour
 
     // 6.5 et -0.9
     void Place(float xjoueur, float yjoueur){
-        if (yjoueur<3.0f && yjoueur>-2.5f && xjoueur>-4.5f && xjoueur<13.5f){
+        if (yjoueur<3.0f && yjoueur>-2.5f && xjoueur>-4.5f && xjoueur<13.5f && notBlocked){
             transform.position = new Vector3(transform.position.x, 7.4f * Mathf.Abs(transform.position.x-xjoueur)/9.0f - 0.9f, transform.position.z);
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.collider.tag == "Ground") notBlocked = false;
+        
     }
 }
