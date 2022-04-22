cf290edbdcd048727eed0c8bb21b9e2daeadf297
victoria0510 <victoria.derul@sciencespo.fr>
2022-04-22 14:29:44 +0200

    probleme de plateforme solution

--- Assets/MirrorPlatform.cs
+++ Assets/MirrorPlatform.cs
@@ -19,6 +19,7 @@ public class MirrorPlatform : MonoBehaviour
         this.GetComponent<BoxCollider2D>().enabled = false;
         gameObject.tag = "MirrorPlatform";
         gameObject.layer = LayerMask.NameToLayer("MirrorPlatform");
+        Debug.Log("Salut, je suis la");
     }
 
     // Update is called once per frame
@@ -30,6 +31,7 @@ public class MirrorPlatform : MonoBehaviour
             this.GetComponent<BoxCollider2D>().enabled = true;
             gameObject.tag = "Ground";
             gameObject.layer = LayerMask.NameToLayer("ground");
+            Debug.Log("Coucou je change le sprite et le bocxollider");
         }
         else
         {
